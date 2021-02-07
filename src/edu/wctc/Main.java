f05b1eab89aff1c212aaeafaeb09e3b5095fa766
Nick Gorjestani <nickg2794@hotmail.com>
2021-02-06 21:18:38 -0600

    Finished Main and fixed bugs

--- src/edu/wctc/Main.java
+++ src/edu/wctc/Main.java
@@ -7,32 +7,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
         Player newPlayer = new Player();
         Maze currentMaze = new Maze(newPlayer);
 
         while (!currentMaze.isFinished()) {
-            Scanner keyboard = new Scanner(System.in);
-
             System.out.println(currentMaze.getCurrentRoomDescription());
             System.out.println("The exits are: " + currentMaze.getCurrentRoomExits());
-            System.out.println("i - Interact\nl - Loot\nv - Check inventory\nn - North\ns - South\ne - East\nw - West\nu - Up\nd - Down");
+            System.out.println("i - Interact\nl - Loot\nv - Check inventory\nn - North\ns - South\ne - East\nw - West\nu - Up\nd - Down\nx - Exit");
             char resp = keyboard.next().charAt(0);
             switch (resp) {
-                case 'i':
-                    currentMaze.interactWithCurrentRoom();
-                case 'l':
-                    currentMaze.lootCurrentRoom();
-                case 'v':
-                    currentMaze.getPlayerInventory();
-                case 'n':
-                case 's':
-                case 'e':
-                case 'w':
-                case 'u':
-                case 'd':
-                    currentMaze.move(resp);
-                default:
-                    System.out.println("That is not a valid option.");
+                case 'i' -> System.out.println(currentMaze.interactWithCurrentRoom());
+                case 'l' -> System.out.println(currentMaze.lootCurrentRoom());
+                case 'v' -> System.out.println(currentMaze.getPlayerInventory());
+                case 'x' -> System.out.println(currentMaze.exitCurrentRoom());
+                case 'n', 's', 'e', 'w', 'u', 'd' -> {
+                    if (!currentMaze.move(resp)) {
+                        System.out.println("You can't move that direction");
+                    }
+                }
+                default -> System.out.println("That is not a valid option.");
             }
         }
     }
